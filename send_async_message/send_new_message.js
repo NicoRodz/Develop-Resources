56a26e0b145aed54feaa4835ae30d2b6c81b82f0
NicoRodz <nicolas.jesusrn@gmail.com>
2021-10-25 16:54:47 -0300

    WIP

--- send_async_message/send_new_message.js
+++ send_async_message/send_new_message.js
@@ -1,20 +1,37 @@
 
-const TASK_NAME = ""
-const QUEUE_NAME = ""
-const ARGS = {}
+const TASK_NAME = "insert_new_task_in_zecore"
+const QUEUE_NAME = "zecore_async_queue"
+const ARGS = {
+    "name": "create_new_order",
+    "quotation_id": 12
+}
 
 const celery = require('celery-node');
 
-const client = celery.createClient(
-    "amqp://guest:guest@localhost:5672",
-    "redis://localhost:6379/0",
-    QUEUE_NAME
-);
 
-const task = client.createTask(TASK_NAME);
-const result = task.applyAsync([JSON.stringify(ARGS)]);
+async function sleep() {
+    return new Promise(() => {
+        process.exit()
+    }, 3500)
+}
 
-result.get().then(data => {
-    console.log(data);
-    client.disconnect();
-})
+(async () => {
+    const client = celery.createClient(
+        "amqp://guest:guest@localhost:5672",
+        "redis://localhost:6379/0",
+        QUEUE_NAME
+    );
+    
+    const task = client.createTask(TASK_NAME);
+    try {
+        const result = await task.applyAsync([JSON.stringify(ARGS)])
+    } catch(e) {
+        console.log(e)
+    }
+})()
+
+// result.get().then(data => {
+//     console.log(data);
+//     client.disconnect();
+// })
+// .catch((e) => console.log(e))
